958b223faf073849d9174baa69222c957c457959
Guto Maia <guto@guto.net>
2012-05-30 09:57:59 -0300

    fix import on example 04

--- examples/04-monitor_log_files.py
+++ examples/04-monitor_log_files.py
@@ -2,24 +2,24 @@
 # coding: utf-8
 
 from fish_slapping import Bot, Log
+from time import strptime
 
 class ApacheLog(Log):
 
     def parse_line(self, line):
         if self.name == "access":
             msgtype = "INFO"
-            tstamp = datetime.strptime(line.split()[3].lstrip('['),
+            tstamp = strptime(line.split()[3].lstrip('['),
                     "%d/%b/%Y:%H:%M:%S")
             msg = line.split("]")[1]
         else:
             msgtype = "ERROR"
-            tstamp = datetime.strptime(line.split(']')[0].lstrip('[') ,
+            tstamp = strptime(line.split(']')[0].lstrip('[') ,
                     "%a %b %d %H:%M:%S %Y")
             msg = line.split("]")[3]
 
         return tstamp, msgtype, msg  
-    
+
 bot = Bot("user@domain.com", "secret_password")
 bot.logs['apache'] = ApacheLog('/var/log/apache2/access.log')
 bot.run()
-    
